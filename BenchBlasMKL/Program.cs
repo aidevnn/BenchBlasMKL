ee264aaff98062143b87231efaf06beba6d42c84
aidevnn <ilyas.kadassi@gmail.com>
2019-06-14 22:21:25 +0300

    float matrix cblas_sgemm

--- BenchBlasMKL/Program.cs
+++ BenchBlasMKL/Program.cs
@@ -8,17 +8,37 @@ namespace BenchBlasMKL
 {
     class MainClass
     {
+        [DllImport("blas", EntryPoint = "cblas_sgemm")]
+        static extern void BLAS_sgemm(int Order, int TransA, int TransB,
+            int M, int N, int K,
+            float alpha, float[] A, int lda, float[] B, int ldb,
+            float beta, float[] C, int ldc);
+
         [DllImport("blas", EntryPoint = "cblas_dgemm")]
         static extern void BLAS_dgemm(int Order, int TransA, int TransB,
             int M, int N, int K,
             double alpha, double[] A, int lda, double[] B, int ldb,
             double beta, double[] C, int ldc);
 
+        [DllImport("mkl_rt", EntryPoint = "cblas_sgemm")]
+        static extern void MKL_sgemm(int Order, int TransA, int TransB,
+            int M, int N, int K,
+            float alpha, float[] A, int lda, float[] B, int ldb,
+            float beta, float[] C, int ldc);
+
         [DllImport("mkl_rt", EntryPoint = "cblas_dgemm")]
         static extern void MKL_dgemm(int Order, int TransA, int TransB,
             int M, int N, int K,
             double alpha, double[] A, int lda, double[] B, int ldb,
             double beta, double[] C, int ldc);
+
+        static void BlasMatrixMultiplication(int m, int n, int k, float[] A, float[] B, float[] C)
+        {
+            if (m * k != A.Length || n * k != B.Length || m * n != C.Length)
+                throw new ArgumentException();
+
+            BLAS_sgemm(101, 111, 111, m, n, k, 1.0f, A, k, B, n, 0.0f, C, n);
+        }
 
         static void BlasMatrixMultiplication(int m, int n, int k, double[] A, double[] B, double[] C)
         {
@@ -28,12 +48,45 @@ namespace BenchBlasMKL
             BLAS_dgemm(101, 111, 111, m, n, k, 1.0, A, k, B, n, 0.0, C, n);
         }
 
+        static void MklMatrixMultiplication(int m, int n, int k, float[] A, float[] B, float[] C)
+        {
+            if (m * k != A.Length || n * k != B.Length || m * n != C.Length)
+                throw new ArgumentException();
+
+            MKL_sgemm(101, 111, 111, m, n, k, 1.0f, A, k, B, n, 0.0f, C, n);
+        }
+
         static void MklMatrixMultiplication(int m, int n, int k, double[] A, double[] B, double[] C)
         {
             if (m * k != A.Length || n * k != B.Length || m * n != C.Length)
                 throw new ArgumentException();
 
             MKL_dgemm(101, 111, 111, m, n, k, 1.0, A, k, B, n, 0.0, C, n);
+        }
+
+        static void DotNetMatrixMultiplication(int m, int n, int k, float[] A, float[] B, float[] C)
+        {
+            if (m * k != A.Length || n * k != B.Length || m * n != C.Length)
+                throw new ArgumentException();
+
+            int i, j, k0, idxA, idxB, idxC;
+            float sum;
+
+            for (i = 0; i < m; ++i)
+            {
+                for (j = 0; j < n; ++j)
+                {
+                    idxC = i * n + j;
+                    sum = 0;
+                    for (k0 = 0; k0 < k; ++k0)
+                    {
+                        idxA = i * k + k0;
+                        idxB = k0 * n + j;
+                        sum += A[idxA] * B[idxB];
+                    }
+                    C[idxC] = sum;
+                }
+            }
         }
 
         static void DotNetMatrixMultiplication(int m, int n, int k, double[] A, double[] B, double[] C)
@@ -61,12 +114,12 @@ namespace BenchBlasMKL
             }
         }
 
-        static void DisplayMatrix(int m, int n, double[] M, string info = "M")
+        static void DisplayMatrix(int m, int n, float[] M, string info = "M")
         {
             if (m * n != M.Length)
                 throw new ArgumentException();
 
-            Console.WriteLine($"Matrix {info} [{m}x{n}]");
+            Console.WriteLine($"MatrixFloat {info} [{m}x{n}]");
             for (int i = 0; i < m; ++i)
             {
                 for (int j = 0; j < n; ++j)
@@ -77,7 +130,39 @@ namespace BenchBlasMKL
             }
         }
 
-        static void TestDotNet()
+        static void DisplayMatrix(int m, int n, double[] M, string info = "M")
+        {
+            if (m * n != M.Length)
+                throw new ArgumentException();
+
+            Console.WriteLine($"MatrixDouble {info} [{m}x{n}]");
+            for (int i = 0; i < m; ++i)
+            {
+                for (int j = 0; j < n; ++j)
+                {
+                    Console.Write($"{M[i * n + j],8:F2}");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static Random random = new Random();
+        static void TestDotNetfloat()
+        {
+            int m = 2, n = 2, k = 2;
+            float[] A = Enumerable.Range(1, 4).Select(i => (float)i).ToArray();
+            float[] B = Enumerable.Range(1, 4).Select(i => -(float)i).ToArray();
+            float[] C = new float[4];
+
+            Console.WriteLine("DotNetMatrixMultiplication");
+            DotNetMatrixMultiplication(m, n, k, A, B, C);
+            DisplayMatrix(m, k, A, "A");
+            DisplayMatrix(k, n, B, "B");
+            DisplayMatrix(m, n, C, "C = A x B");
+            Console.WriteLine();
+        }
+
+        static void TestDotNetdouble()
         {
             int m = 2, n = 2, k = 2;
             double[] A = Enumerable.Range(1, 4).Select(i => (double)i).ToArray();
@@ -92,7 +177,22 @@ namespace BenchBlasMKL
             Console.WriteLine();
         }
 
-        static void TestMKL()
+        static void TestMKLfloat()
+        {
+            int m = 2, n = 2, k = 2;
+            float[] A = Enumerable.Range(1, 4).Select(i => (float)i).ToArray();
+            float[] B = Enumerable.Range(1, 4).Select(i => -(float)i).ToArray();
+            float[] C = new float[4];
+
+            Console.WriteLine("MklMatrixMultiplication");
+            MklMatrixMultiplication(m, n, k, A, B, C);
+            DisplayMatrix(m, k, A, "A");
+            DisplayMatrix(k, n, B, "B");
+            DisplayMatrix(m, n, C, "C = A x B");
+            Console.WriteLine();
+        }
+
+        static void TestMKLdouble()
         {
             int m = 2, n = 2, k = 2;
             double[] A = Enumerable.Range(1, 4).Select(i => (double)i).ToArray();
@@ -107,7 +207,22 @@ namespace BenchBlasMKL
             Console.WriteLine();
         }
 
-        static void TestBLAS()
+        static void TestBLASfloat()
+        {
+            int m = 2, n = 2, k = 2;
+            float[] A = Enumerable.Range(1, 4).Select(i => (float)i).ToArray();
+            float[] B = Enumerable.Range(1, 4).Select(i => -(float)i).ToArray();
+            float[] C = new float[4];
+
+            Console.WriteLine("BlasMatrixMultiplication");
+            BlasMatrixMultiplication(m, n, k, A, B, C);
+            DisplayMatrix(m, k, A, "A");
+            DisplayMatrix(k, n, B, "B");
+            DisplayMatrix(m, n, C, "C = A x B");
+            Console.WriteLine();
+        }
+
+        static void TestBLASdouble()
         {
             int m = 2, n = 2, k = 2;
             double[] A = Enumerable.Range(1, 4).Select(i => (double)i).ToArray();
@@ -122,42 +237,21 @@ namespace BenchBlasMKL
             Console.WriteLine();
         }
 
-        static Random random = new Random();
-        static void Bench1(int m, int n, int k, bool display = false)
+        static void BenchDotNetfloat(int m, int n, int k)
         {
             if (m <= 0 || n <= 0 || k <= 0)
                 throw new ArgumentException();
 
-            double[] A = Enumerable.Range(0, m * k).Select(i => random.NextDouble() * 2 - 1).ToArray();
-            double[] B = Enumerable.Range(0, k * n).Select(i => random.NextDouble() * 2 - 1).ToArray();
-            double[] C0 = new double[m * n];
-            double[] C1 = new double[m * n];
-            double[] C2 = new double[m * n];
+            float[] A = Enumerable.Range(0, m * k).Select(i => (float)random.NextDouble() * 2 - 1).ToArray();
+            float[] B = Enumerable.Range(0, k * n).Select(i => (float)random.NextDouble() * 2 - 1).ToArray();
+            float[] C = new float[m * n];
 
             var sw0 = Stopwatch.StartNew();
-            DotNetMatrixMultiplication(m, n, k, A, B, C0);
-            Console.WriteLine($"DotNetMatrixMultiplication ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
-
-            var sw1 = Stopwatch.StartNew();
-            BlasMatrixMultiplication(m, n, k, A, B, C1);
-            Console.WriteLine($"BlasMatrixMultiplication   ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw1.ElapsedMilliseconds} ms");
-
-            var sw2 = Stopwatch.StartNew();
-            MklMatrixMultiplication(m, n, k, A, B, C2);
-            Console.WriteLine($"MklMatrixMultiplication    ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw2.ElapsedMilliseconds} ms");
-
-            Console.WriteLine();
-
-            if (display)
-            {
-                DisplayMatrix(m, n, C0, "Dotnet");
-                DisplayMatrix(m, n, C1, "BLAS");
-                DisplayMatrix(m, n, C2, "MKL");
-                Console.WriteLine();
-            }
+            DotNetMatrixMultiplication(m, n, k, A, B, C);
+            Console.WriteLine($"DotNetMatrixMultiplication float ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
         }
 
-        static void BenchDotNet(int m, int n, int k)
+        static void BenchDotNetdouble(int m, int n, int k)
         {
             if (m <= 0 || n <= 0 || k <= 0)
                 throw new ArgumentException();
@@ -168,10 +262,24 @@ namespace BenchBlasMKL
 
             var sw0 = Stopwatch.StartNew();
             DotNetMatrixMultiplication(m, n, k, A, B, C);
-            Console.WriteLine($"DotNetMatrixMultiplication ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
+            Console.WriteLine($"DotNetMatrixMultiplication double ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
         }
 
-        static void BenchBLAS(int m, int n, int k)
+        static void BenchBLASfloat(int m, int n, int k)
+        {
+            if (m <= 0 || n <= 0 || k <= 0)
+                throw new ArgumentException();
+
+            float[] A = Enumerable.Range(0, m * k).Select(i => (float)random.NextDouble() * 2 - 1).ToArray();
+            float[] B = Enumerable.Range(0, k * n).Select(i => (float)random.NextDouble() * 2 - 1).ToArray();
+            float[] C = new float[m * n];
+
+            var sw0 = Stopwatch.StartNew();
+            BlasMatrixMultiplication(m, n, k, A, B, C);
+            Console.WriteLine($"BlasMatrixMultiplication   float ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
+        }
+
+        static void BenchBLASdouble(int m, int n, int k)
         {
             if (m <= 0 || n <= 0 || k <= 0)
                 throw new ArgumentException();
@@ -182,10 +290,24 @@ namespace BenchBlasMKL
 
             var sw0 = Stopwatch.StartNew();
             BlasMatrixMultiplication(m, n, k, A, B, C);
-            Console.WriteLine($"BlasMatrixMultiplication   ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
+            Console.WriteLine($"BlasMatrixMultiplication   double ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
         }
 
-        static void BenchMKL(int m, int n, int k)
+        static void BenchMKLfloat(int m, int n, int k)
+        {
+            if (m <= 0 || n <= 0 || k <= 0)
+                throw new ArgumentException();
+
+            float[] A = Enumerable.Range(0, m * k).Select(i => (float)random.NextDouble() * 2 - 1).ToArray();
+            float[] B = Enumerable.Range(0, k * n).Select(i => (float)random.NextDouble() * 2 - 1).ToArray();
+            float[] C = new float[m * n];
+
+            var sw0 = Stopwatch.StartNew();
+            MklMatrixMultiplication(m, n, k, A, B, C);
+            Console.WriteLine($"MklMatrixMultiplication    float ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
+        }
+
+        static void BenchMKLdouble(int m, int n, int k)
         {
             if (m <= 0 || n <= 0 || k <= 0)
                 throw new ArgumentException();
@@ -196,40 +318,50 @@ namespace BenchBlasMKL
 
             var sw0 = Stopwatch.StartNew();
             MklMatrixMultiplication(m, n, k, A, B, C);
-            Console.WriteLine($"MklMatrixMultiplication    ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
+            Console.WriteLine($"MklMatrixMultiplication    double ({m} {k}) x ({k} {n}) = ({m} {n}) Time = {sw0.ElapsedMilliseconds} ms");
         }
 
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!\n");
+            TestDotNetdouble();
+            TestBLASdouble();
+            TestMKLdouble();
 
-            TestDotNet();
-            TestMKL();
-            TestBLAS();
+            TestDotNetfloat();
+            TestBLASfloat();
+            TestMKLfloat();
 
             int N = 4;
             int c = 2;
-            for (int k = 0; k < N; ++k)
-            {
-                BenchDotNet(30 * c, 40 * c, 20 * c);
-                c *= 2;
-            }
+            for (int k = 0; k < N; ++k, c *= 2)
+                BenchDotNetfloat(30 * c, 40 * c, 20 * c);
             Console.WriteLine();
 
             c = 16;
-            for (int k = 0; k < N; ++k)
-            {
-                BenchBLAS(30 * c, 40 * c, 20 * c);
-                c *= 2;
-            }
+            for (int k = 0; k < N; ++k, c *= 2)
+                BenchBLASfloat(30 * c, 40 * c, 20 * c);
             Console.WriteLine();
 
             c = 16;
-            for (int k = 0; k < N; ++k)
-            {
-                BenchMKL(30 * c, 40 * c, 20 * c);
-                c *= 2;
-            }
+            for (int k = 0; k < N; ++k, c *= 2)
+                BenchMKLfloat(30 * c, 40 * c, 20 * c);
+            Console.WriteLine();
+
+            c = 2;
+            for (int k = 0; k < N; ++k, c *= 2)
+                BenchDotNetdouble(30 * c, 40 * c, 20 * c);
+            Console.WriteLine();
+
+            c = 16;
+            for (int k = 0; k < N; ++k, c *= 2)
+                BenchBLASdouble(30 * c, 40 * c, 20 * c);
+            Console.WriteLine();
+
+            c = 16;
+            for (int k = 0; k < N; ++k, c *= 2)
+                BenchMKLdouble(30 * c, 40 * c, 20 * c);
+            Console.WriteLine();
+
         }
     }
 }
--- README.md
+++ README.md
@@ -6,51 +6,101 @@ Assuming libblas.so and libmkl_rt.so are avalaible from ldconfig.
 ### Output
 ```
 DotNetMatrixMultiplication
-Matrix A [2x2]
+MatrixDouble A [2x2]
     1.00    2.00
     3.00    4.00
-Matrix B [2x2]
+MatrixDouble B [2x2]
    -1.00   -2.00
    -3.00   -4.00
-Matrix C = A x B [2x2]
-   -7.00  -10.00
-  -15.00  -22.00
-
-MklMatrixMultiplication
-Matrix A [2x2]
-    1.00    2.00
-    3.00    4.00
-Matrix B [2x2]
-   -1.00   -2.00
-   -3.00   -4.00
-Matrix C = A x B [2x2]
+MatrixDouble C = A x B [2x2]
    -7.00  -10.00
   -15.00  -22.00
 
 BlasMatrixMultiplication
-Matrix A [2x2]
+MatrixDouble A [2x2]
     1.00    2.00
     3.00    4.00
-Matrix B [2x2]
+MatrixDouble B [2x2]
    -1.00   -2.00
    -3.00   -4.00
-Matrix C = A x B [2x2]
+MatrixDouble C = A x B [2x2]
    -7.00  -10.00
   -15.00  -22.00
 
-DotNetMatrixMultiplication (60 40) x (40 80) = (60 80) Time = 3 ms
-DotNetMatrixMultiplication (120 80) x (80 160) = (120 160) Time = 17 ms
-DotNetMatrixMultiplication (240 160) x (160 320) = (240 320) Time = 83 ms
-DotNetMatrixMultiplication (480 320) x (320 640) = (480 640) Time = 548 ms
+MklMatrixMultiplication
+MatrixDouble A [2x2]
+    1.00    2.00
+    3.00    4.00
+MatrixDouble B [2x2]
+   -1.00   -2.00
+   -3.00   -4.00
+MatrixDouble C = A x B [2x2]
+   -7.00  -10.00
+  -15.00  -22.00
 
-BlasMatrixMultiplication   (480 320) x (320 640) = (480 640) Time = 5 ms
-BlasMatrixMultiplication   (960 640) x (640 1280) = (960 1280) Time = 22 ms
-BlasMatrixMultiplication   (1920 1280) x (1280 2560) = (1920 2560) Time = 181 ms
-BlasMatrixMultiplication   (3840 2560) x (2560 5120) = (3840 5120) Time = 1512 ms
+DotNetMatrixMultiplication
+MatrixFloat A [2x2]
+    1.00    2.00
+    3.00    4.00
+MatrixFloat B [2x2]
+   -1.00   -2.00
+   -3.00   -4.00
+MatrixFloat C = A x B [2x2]
+   -7.00  -10.00
+  -15.00  -22.00
 
-MklMatrixMultiplication    (480 320) x (320 640) = (480 640) Time = 7 ms
-MklMatrixMultiplication    (960 640) x (640 1280) = (960 1280) Time = 28 ms
-MklMatrixMultiplication    (1920 1280) x (1280 2560) = (1920 2560) Time = 152 ms
-MklMatrixMultiplication    (3840 2560) x (2560 5120) = (3840 5120) Time = 1223 ms
+BlasMatrixMultiplication
+MatrixFloat A [2x2]
+    1.00    2.00
+    3.00    4.00
+MatrixFloat B [2x2]
+   -1.00   -2.00
+   -3.00   -4.00
+MatrixFloat C = A x B [2x2]
+   -7.00  -10.00
+  -15.00  -22.00
+
+MklMatrixMultiplication
+MatrixFloat A [2x2]
+    1.00    2.00
+    3.00    4.00
+MatrixFloat B [2x2]
+   -1.00   -2.00
+   -3.00   -4.00
+MatrixFloat C = A x B [2x2]
+   -7.00  -10.00
+  -15.00  -22.00
+
+
+DotNetMatrixMultiplication float (60 40) x (40 80) = (60 80) Time = 1 ms
+DotNetMatrixMultiplication float (120 80) x (80 160) = (120 160) Time = 10 ms
+DotNetMatrixMultiplication float (240 160) x (160 320) = (240 320) Time = 83 ms
+DotNetMatrixMultiplication float (480 320) x (320 640) = (480 640) Time = 486 ms
+
+BlasMatrixMultiplication   float (480 320) x (320 640) = (480 640) Time = 8 ms
+BlasMatrixMultiplication   float (960 640) x (640 1280) = (960 1280) Time = 12 ms
+BlasMatrixMultiplication   float (1920 1280) x (1280 2560) = (1920 2560) Time = 107 ms
+BlasMatrixMultiplication   float (3840 2560) x (2560 5120) = (3840 5120) Time = 859 ms
+
+MklMatrixMultiplication    float (480 320) x (320 640) = (480 640) Time = 8 ms
+MklMatrixMultiplication    float (960 640) x (640 1280) = (960 1280) Time = 10 ms
+MklMatrixMultiplication    float (1920 1280) x (1280 2560) = (1920 2560) Time = 116 ms
+MklMatrixMultiplication    float (3840 2560) x (2560 5120) = (3840 5120) Time = 626 ms
+
+DotNetMatrixMultiplication double (60 40) x (40 80) = (60 80) Time = 0 ms
+DotNetMatrixMultiplication double (120 80) x (80 160) = (120 160) Time = 8 ms
+DotNetMatrixMultiplication double (240 160) x (160 320) = (240 320) Time = 58 ms
+DotNetMatrixMultiplication double (480 320) x (320 640) = (480 640) Time = 539 ms
+
+BlasMatrixMultiplication   double (480 320) x (320 640) = (480 640) Time = 4 ms
+BlasMatrixMultiplication   double (960 640) x (640 1280) = (960 1280) Time = 24 ms
+BlasMatrixMultiplication   double (1920 1280) x (1280 2560) = (1920 2560) Time = 227 ms
+BlasMatrixMultiplication   double (3840 2560) x (2560 5120) = (3840 5120) Time = 1582 ms
+
+MklMatrixMultiplication    double (480 320) x (320 640) = (480 640) Time = 11 ms
+MklMatrixMultiplication    double (960 640) x (640 1280) = (960 1280) Time = 20 ms
+MklMatrixMultiplication    double (1920 1280) x (1280 2560) = (1920 2560) Time = 174 ms
+MklMatrixMultiplication    double (3840 2560) x (2560 5120) = (3840 5120) Time = 1330 ms
+
 
 ```
